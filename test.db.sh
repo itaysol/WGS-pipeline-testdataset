402369260ba9c9f21c6f58e2c6ae372771682dd7
Yair Motro <yair.motro@gmail.com>
2023-06-11 09:48:53 +0000

    added test kraken2 database download and ignore

---
+++ test.db.sh
@@ -0,0 +1,4 @@
+mkdir -p databases/k2
+wget -c https://genome-idx.s3.amazonaws.com/kraken/minikraken2_v2_8GB_201904.tgz -O databases/k2/minikraken2_v2_8GB_201904.tgz
+tar xzvf databases/k2/minikraken2_v2_8GB_201904.tgz -C databases/k2
+rm -f databases/k2/minikraken2_v2_8GB_201904.tgz
