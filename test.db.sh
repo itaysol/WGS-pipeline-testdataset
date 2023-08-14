de8400a2c1455ea5d665f2105d467e8c2748a3bd
itay sol <itaysol97@gmail.com>
2023-08-14 14:13:22 +0000

    changed the config file and set the snakefile to run on all the samples in it

--- test.db.sh
+++ test.db.sh
@@ -1,4 +1,5 @@
 mkdir -p databases/k2
+pip install pyyaml
 wget -c https://genome-idx.s3.amazonaws.com/kraken/minikraken2_v2_8GB_201904.tgz -O databases/k2/minikraken2_v2_8GB_201904.tgz
 tar xzvf databases/k2/minikraken2_v2_8GB_201904.tgz -C databases/k2
 rm -f databases/k2/minikraken2_v2_8GB_201904.tgz
