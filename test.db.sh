022bff08ad887895636f27d61da7bb88ac4f669c
itaysol <94539258+itaysol@users.noreply.github.com>
2023-11-06 11:22:08 +0200

    Update test.db.sh

--- test.db.sh
+++ test.db.sh
@@ -1,5 +1,4 @@
 mkdir -p databases/k2
-mkdir -p databases/amrfinderplus
 pip install pyyaml
 wget -c https://genome-idx.s3.amazonaws.com/kraken/k2_standard_08gb_20231009.tar.gz -O databases/krakenStandardDB.tgz
 tar xzvf databases/krakenStandardDB.tgz -C databases/k2
