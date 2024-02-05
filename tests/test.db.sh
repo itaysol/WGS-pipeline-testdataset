f449988c55d4aa7ff722d9314e293927354aa192
Yair Motro <31047716+motroy@users.noreply.github.com>
2024-02-05 12:06:55 +0200

    Delete tests/test.db.sh

--- tests/test.db.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p databases/k2
-pip install pyyaml
-wget -c https://genome-idx.s3.amazonaws.com/kraken/k2_standard_08gb_20231009.tar.gz -O databases/krakenStandardDB.tgz
-tar xzvf databases/krakenStandardDB.tgz -C databases/k2
-rm -f databases/krakenStandardDB.tgz
